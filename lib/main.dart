bd244a9bc0c9bc73b59a3a2d4a872e6325c99f16
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-09 18:10:30 -0400

    Initial Firebase/Firestore setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ import 'package:character_app/theme.dart';
 import 'package:character_app/screens/home/home.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(ChangeNotifierProvider(
     create: (context) => CharacterStore(),
     child: MaterialApp(
